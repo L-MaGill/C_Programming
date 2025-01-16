30e4a43062a2046303f9ca4415e108005b9bb077
L-MaGill <lmagill9788@floridapoly.edu>
2025-01-16 16:29:08 +0000

    Impliment First Program

---
+++ 01_first_program/first_program.c
@@ -0,0 +1,20 @@
+// one-line comment 
+// this is the first program which prints a welcoming message
+
+/*
+
+Multi Line comment. 
+
+Today is January 9th 2025.
+
+*/ 
+
+// create a main function
+#include <stdio.h>
+
+int main() {
+    printf("I\nlove\nducks!\n");
+    printf("Ducks\nare\nreally\ncool!\n");
+
+    return 0;
+}
